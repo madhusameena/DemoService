07eef2d1bf9fed65e7376555556b89c18ec4a945
Shyam Mergu <Shyamchinna5@gmail.com>
2018-09-28 13:50:10 +0530

    Created Models for Database classes

---
+++ DemoService/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoService.Models
+{
+    public partial class Project
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public byte[] Img { get; set; }
+        public int? UserId { get; set; }
+
+        public Userdetails User { get; set; }
+    }
+}
---
+++ DemoService/Models/Userdetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoService.Models
+{
+    public partial class Userdetails
+    {
+        public Userdetails()
+        {
+            Project = new HashSet<Project>();
+        }
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+
+        public ICollection<Project> Project { get; set; }
+    }
+}
